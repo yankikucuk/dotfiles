c2ef9b8e25c2034b31c77e06455db483eae80077
yankikucuk <kendinikertenkelebek@me.com>
2024-09-20 03:21:12 +0300

    added headless os

--- .config/server/install.sh
+++ .config/server/install.sh
@@ -2,4 +2,13 @@
 
 # this script provides headless setup for use cases.
 
-echo "Hello world from Server"
+apt-get update
+apt-get upgrade
+apt install curl
+apt install git
+sudo apt-get install build-essential
+brew install gcc
+brew install node@20
+brew install corepack
+brew install wget
+corepack use pnpm@latest
